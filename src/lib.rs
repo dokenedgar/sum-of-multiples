c6f5bdf85ba1bea570d096e6e59d57ca736df80a
Doken Edgar <dokenedgarjr@gmail.com>
2025-01-29 22:17:50 +0100

    Add solution to the exercise

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    todo!("Sum the multiples of all of {factors:?} which are less than {limit}")
+    let mut factors_vector: Vec<u32> = Vec::new();
+    let mut counter = 1;
+    for item in factors {
+        if *item < 1 {
+            break;
+        }
+        while counter * item < limit {
+            if !factors_vector.contains(&(item * counter)) {
+                factors_vector.push(counter * item);
+            }
+            counter += 1;
+        }
+        counter = 1;
+    }
+    factors_vector.sort();
+    factors_vector
+        .iter()
+        .copied()
+        .reduce(|a, b| a + b)
+        .unwrap_or(0)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,5 @@
-fn main() {}
+use sum_of_multiples::sum_of_multiples;
+fn main() {
+    let result = sum_of_multiples(1, &[0]);
+    println!("{result}");
+}
